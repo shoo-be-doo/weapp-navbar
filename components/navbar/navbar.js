077a5c33a19d83f2f3f9a583742bedd421ec8222
Shoo_Be_Doo <sjp806470205@gmail.com>
2019-05-21 17:00:30 +0800

    增加初始化样式配置

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,43 +1,72 @@
-// components/navbar/navbar.js
+// 系统信息
+const systemInfo = wx.getSystemInfoSync();
+// 胶囊按钮信息
+const menuButtonRect = wx.getMenuButtonBoundingClientRect();
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
+  	// 背景
+	  background: {
+	  	type: String,
+		  value: '#FFF'
+	  },
 
+	  // 字体颜色
+	  textColor: {
+		  type: String,
+		  value: '#000'
+	  },
+
+	  // 标题
+	  title: {
+		  type: String,
+		  value: '标题'
+	  },
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-	  navbarHeight: 0
+	  navbarStyle: ''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-  	// 计算自定义导航栏高度
-		getNavbarHeight() {
-			// 系统信息
-			const systemInfo = wx.getSystemInfoSync();
-			// 胶囊按钮信息
-			const menuButtonRect = wx.getMenuButtonBoundingClientRect();
-			// 状态栏高度
-			const { statusBarHeight } = systemInfo;
-			// 自定义导航栏高度
-			const navbarHeight = (menuButtonRect.top - statusBarHeight) * 2 + menuButtonRect.height + menuButtonRect.top;
-			this.setData({
-				navbarHeight
-			});
-		}
+	  // 初始化自定义导航栏样式
+	  initNavbarLayout() {
+		  const { top, right, width, height } = menuButtonRect;
+		  const { statusBarHeight, windowWidth, fontSizeSetting } = systemInfo;
+		  // 自定义导航栏高度
+		  const navbarHeight = top - statusBarHeight + height + top;
+		  // 内边距
+		  let paddingBottom = top - statusBarHeight;
+		  let paddingTop = statusBarHeight + paddingBottom;
+		  let paddingLeft = windowWidth - right;
+		  let paddingRight = paddingLeft * 2 + width;
+		  let navbarStyle = `
+				padding-top: ${ paddingTop }px;
+				padding-right: ${ paddingRight }px;
+				padding-bottom: ${ paddingBottom }px;
+				padding-left: ${ paddingLeft }px;
+				height: ${ navbarHeight }px;
+				font-size: ${ fontSizeSetting }px;
+				background: ${ this.properties.background };
+			`;
+		  this.setData({
+			  navbarStyle,
+		  });
+	  },
   },
 
 	/**
 	 * 组件生命周期函数-在组件实例进入页面节点树时执行
 	 */
 	attached() {
-		this.getNavbarHeight();
+		this.initNavbarLayout();
 	}
 });

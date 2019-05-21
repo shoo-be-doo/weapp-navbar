e721eef748bfe5e212162ae8ba00b47dd905b6bb
Shoo_Be_Doo <sjp806470205@gmail.com>
2019-05-21 16:21:05 +0800

    增加计算自定义导航栏高度的方法

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -11,13 +11,33 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+	  navbarHeight: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+  	// 计算自定义导航栏高度
+		getNavbarHeight() {
+			// 系统信息
+			const systemInfo = wx.getSystemInfoSync();
+			// 胶囊按钮信息
+			const menuButtonRect = wx.getMenuButtonBoundingClientRect();
+			// 状态栏高度
+			const { statusBarHeight } = systemInfo;
+			// 自定义导航栏高度
+			const navbarHeight = (menuButtonRect.top - statusBarHeight) * 2 + menuButtonRect.height + menuButtonRect.top;
+			this.setData({
+				navbarHeight
+			});
+		}
+  },
 
-  }
-})
+	/**
+	 * 组件生命周期函数-在组件实例进入页面节点树时执行
+	 */
+	attached() {
+		this.getNavbarHeight();
+	}
+});
